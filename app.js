4ae5d01d3cbff68d453dff103aba134362b0dac1
Nick Kinlen <nick_kinlen@hotmail.com>
2017-09-04 22:06:45 -0400

    Clock initializing function halfway built

--- app.js
+++ app.js
@@ -3,31 +3,38 @@ var breakTime = 5.00;
 
 $(document).ready(function(){
 
+var breaking = false;
+var working = true;
 
 // Initialize the clock
-function initializeClock(time){
+function initializeClock(id, endtime){
   var clock = $('#timeRemaining');
 
   var minutes = ('.minutes');
   var seconds = ('.seconds');
 
+  clock.text(endtime);
 }
 
-console.log(initializeClock(workTime));
+initializeClock('#timeRemaining', workTime);
 
+if(workTime === 0){
+  breaking = true;
+
+  initializeClock('#timeRemaining', breakTime);
+}
+
+console.log(breaking);
 
 // Add/subtract time to break
 $('#subtractBreakMinute').click(function(){
-
   if(breakTime > 0) {
     breakTime -= 1;
     $('#breakDisplay').text(breakTime);
   }
 })
 
-
 $('#addBreakMinute').click(function(){
-
   if(breakTime < 59) {
     breakTime += 1;
     $('#breakDisplay').text(breakTime);
@@ -36,7 +43,6 @@ $('#addBreakMinute').click(function(){
 
 // Add/subtract time to workTime
 $('#subtractSessionMinute').click(function(){
-
   if(workTime > 0) {
     workTime -= 1;
     $('#sessionDisplay').text(workTime);
@@ -44,11 +50,12 @@ $('#subtractSessionMinute').click(function(){
 })
 
 $('#addSessionMinute').click(function(){
-
   if(workTime < 59) {
     workTime += 1;
     $('#sessionDisplay').text(workTime);
   }
 })
+
+
 
 });
