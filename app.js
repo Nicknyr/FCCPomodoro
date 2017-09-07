3bbdbec0c9a89a857b97ef28c662a47a2522b5ab
Nick Kinlen <nick_kinlen@hotmail.com>
2017-09-07 16:08:35 -0400

    Before debugging clock issue

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 var workTime = 25.00;
 var breakTime = 5.00;
+var breaking = false;
+var working = true;
+
 
 $(document).ready(function(){
 
-var breaking = false;
-var working = true;
+function getTimeRemaining(endtime){
+    var minutes = endtime;
+    var seconds = endtime * 60;
+    var secondsCounter = 0;
+
+    return {
+      'minutes': minutes,
+      'seconds': seconds
+    };
+
+}
 
 // Initialize the clock
 function initializeClock(id, endtime){
   var clock = $('#timeRemaining');
+  clock.text(workTime);
 
-  var minutes = ('.minutes');
-  var seconds = ('.seconds');
+  var minutesDiv = $('.minutes');
+  var secondsDiv = $('.seconds');
 
-  clock.text(endtime);
+  // Should display the updated time every second
+  function updateClock(){
+
+    var t = getTimeRemaining(endtime);
+
+    // These should display the time in the clock, but aren't working
+    clock.text = ('0' + t.minutes).slice(-2);
+    clock.text = ('0' + t.seconds).slice(-2);
+
+
+    console.log(t.seconds);
+
+    if (t.seconds <= 0) {
+      clearInterval(timeInterval);
+    }
+  }
+  updateClock();
+  var timeInterval = setInterval(updateClock, 1000);
+
 }
 
-initializeClock('#timeRemaining', workTime);
+  initializeClock('#timeRemaining', workTime);
 
 if(workTime === 0){
   breaking = true;
@@ -24,7 +55,6 @@ if(workTime === 0){
   initializeClock('#timeRemaining', breakTime);
 }
 
-console.log(breaking);
 
 // Add/subtract time to break
 $('#subtractBreakMinute').click(function(){
@@ -55,7 +85,6 @@ $('#addSessionMinute').click(function(){
     $('#sessionDisplay').text(workTime);
   }
 })
-
 
 
 });
